577e509c0c9f5ae3c3322d85be0ce644be773f79
fred <fredwright95@yahoo.com>
2022-04-25 14:12:03 -0400

    prob 16

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -230,6 +230,10 @@ namespace DatabaseFirstLINQ
         private void ProblemSixteen()
         {
             // Update the price of the product you created to something different using LINQ.
+            var product = _context.Products.Where(p => p.Name == "BaseBall card").SingleOrDefault();
+            product.Price = 600;
+            _context.Products.Update(product);
+            _context.SaveChanges();
 
         }
 
